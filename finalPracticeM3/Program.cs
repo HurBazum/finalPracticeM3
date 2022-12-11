dfadc448becfe21f1bd93863a83fe7796ee483a3
Anton Spiridonov <hoorbazoom@gmail.com>
2022-12-11 11:04:11 +0700

    Добавьте файлы проекта.

---
+++ finalPracticeM3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя: ");
+string myName = Console.ReadLine();
+
+Console.Write("Введите возраст: ");
+int myAge = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Ваше имя {myName}, Вам {myAge} лет.");
+
+Console.Write("Введите дату рождения: ");
+DateTime myBirthDate = Convert.ToDateTime(Console.ReadLine());
+
+Console.WriteLine($"Дата Вашего роождения - {myBirthDate.ToShortDateString()}");
